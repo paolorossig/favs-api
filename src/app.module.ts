c3a0e44f116c149907dc1386902a2f8ca8e47077
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-04-12 21:15:06 -0500

    feat: add env config, db connection & validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FavsModule } from './favs/favs.module';
 
 @Module({
-  imports: [FavsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URI),
+    FavsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
