b577ab580b4c18dc6e902c2f95eb50a13d77f468
PaoloRossi <paolo.rossi@pucp.edu.pe>
2022-04-18 00:08:11 -0500

    feat: add api routes and remove app unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,22 @@
 import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FavsModule } from './favs/favs.module';
 import { AuthModule } from './auth/auth.module';
+import { routes } from './routes';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.DB_URI),
+    RouterModule.register(routes),
     UsersModule,
     FavsModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
